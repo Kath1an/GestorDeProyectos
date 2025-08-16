c26ca9f8f0e0eabfb40a5e9f17d6e428e63738ca
Kathian <cristhianmarquez72@gmail.com>
2025-08-15 19:05:00 -0500

    agregada la funcion de imprimir estudiantes

--- estudiante.py
+++ estudiante.py
@@ -1,9 +1,29 @@
 from datetime import datetime
+proyeto="proyetos.txt"
+estudiantes="registro.txt"
 def crearProyecto():
     print("=====Creacion de proyectos=====")
     proyecto=input("Ingrese el nombre del Proyecto: ")
     costo=input("Ingrese el costo del proyecto: ")
-    
-def fechaValida():
-    try: 
-        datetime.strptime(fecha_str, %d/%m/d
+    fehceInicio=input("Ingrese la fecha de inicio del proyecto (dd/mm/yyyy): ")
+    fechaFin=input("Ingrese la fecha de finalizacion del proyecto (dd/mm/yyyy): ")
+    tareas=[]
+    while True:
+        tarea=input("Ingrese una tarea (o deje en blanco para terminar): ")
+        if tarea=="":
+            break
+        else:
+            verEstudiantes()
+
+    with open(proyeto, "a") as archivo:
+        archivo.write(f"{proyecto},{costo},{fehceInicio},{fechaFin}\n")
+        print("Proyecto creado exitosamente.")
+
+def verEstudiantes():
+            with open(estudiantes, "r") as archivo:
+                for linea in archivo:
+                    datos = linea.strip().split(",")
+                    if datos[5]=="Estudiante":
+                        print(f"Cedula: {datos[2]} Nombre: {datos[0].title()} {datos[1].title()}")
+                      
+verEstudiantes()
